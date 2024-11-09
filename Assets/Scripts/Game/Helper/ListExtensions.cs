f7da1bbe5c9303ad183017be35438d52b3bd480a
josemgmz <jmgc92@gmail.com>
2024-11-09 21:49:02 +0100

    feat: connect cardgrid to event bus in order to receive event to create new grids

---
+++ Assets/Scripts/Game/Helper/ListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Helper
+{
+    
+    public static class ListExtensions
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            var rng = new Random();
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+    }
+}
