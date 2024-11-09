fc6fc00eade7b1209e484e6444952b9d93606d35
josemgmz <jmgc92@gmail.com>
2024-11-09 12:08:57 +0100

    refactor: MVC async calls and GameAddressableContext

--- Assets/Scripts/Framework/GameView.cs
+++ Assets/Scripts/Framework/GameView.cs
@@ -111,7 +111,7 @@ namespace Framework
             });
         }
 
-        private async void InitializeControllerAttributes(bool force = false)
+        private void InitializeControllerAttributes(bool force = false)
         {
             _gameMethods = new GameMethods();
             _rawControllers = new Dictionary<Type, object>();
