5b54f47426ec6ecc1a60a5cb88faaf002df13500
josemgmz <jmgc92@gmail.com>
2024-11-10 11:33:33 +0100

    feat: add UITextElement that will display the game state with properties like score, time, turns and combo.
    (cherry picked from commit 74500696c935933403c7d3cf24c2cb94971b4ded)

---
+++ Assets/Scripts/Game/Entities/UITextElement/UITextElementController.cs
@@ -0,0 +1,21 @@
+﻿using Framework;
+
+namespace Game.Entities.UITextElement
+{
+    public class UITextElementController : GameController<UITextElementView>
+    {
+        #region Public Methods
+        
+        public void SetTitle(string title)
+        {
+            GetModel<UITextElementModel>().Title.text = title;
+        }
+        
+        public void SetValue(string value)
+        {
+            GetModel<UITextElementModel>().Value.text = value;
+        }
+        
+        #endregion
+    }
+}
