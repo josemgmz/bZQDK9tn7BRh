709ff8192fb5d7a3875395db9511118024f0c67f
josemgmz <jmgc92@gmail.com>
2024-11-11 00:48:42 +0100

    docs: card controllers documentation

--- Assets/Scripts/Game/Entities/Card/CardEventController.cs
+++ Assets/Scripts/Game/Entities/Card/CardEventController.cs
@@ -6,30 +6,45 @@ using VContainer;
 
 namespace Game.Entities.Card
 {
+    /// <summary>
+    /// Controls the card events, handling card flip and match events.
+    /// </summary>
     public class CardEventController : GameController<CardView>
     {
         #region Services
-        
+
+        /// <summary>
+        /// Event bus for handling game events.
+        /// </summary>
         [Inject] private IGameEventBus _gameEventBus;
 
         #endregion
 
         #region Lifecycle
 
+        /// <summary>
+        /// Registers event listeners when the controller is awakened.
+        /// </summary>
         private void Awake()
         {
             _gameEventBus.AddListener<OnRoundStartEvent>(OnCardFlipEvent);
         }
 
+        /// <summary>
+        /// Unregisters event listeners when the controller is destroyed.
+        /// </summary>
         public void OnDestroy()
         {
             _gameEventBus.RemoveListener<OnRoundStartEvent>(OnCardFlipEvent);
         }
 
         #endregion
-        
+
         #region Public Methods
 
+        /// <summary>
+        /// Sends the card flipped event.
+        /// </summary>
         public void SendOnCardFlippedEvent()
         {
             var model = GetModel<CardModel>();
@@ -46,19 +61,28 @@ namespace Game.Entities.Card
         #endregion
 
         #region Private Methods
-        
+
+        /// <summary>
+        /// Handles the failure of a card flip event.
+        /// </summary>
         private async void OnFail()
         {
             await Task.Delay(250);
             GetController<CardAnimationController>().FlipCard();
         }
-        
+
+        /// <summary>
+        /// Handles the success of a card flip event.
+        /// </summary>
         private async void OnSuccess()
         {
             await Task.Delay(250);
             GetController<CardAnimationController>().MatchCard();
         }
 
+        /// <summary>
+        /// Handles the card flip event.
+        /// </summary>
         private void OnCardFlipEvent()
         {
             GetController<CardAnimationController>().FlipCard(false);
--- Assets/Scripts/Game/Entities/Card/CardImageController.cs
+++ Assets/Scripts/Game/Entities/Card/CardImageController.cs
@@ -6,19 +6,28 @@ using VContainer;
 
 namespace Game.Entities.Card
 {
+    /// <summary>
+    /// Controls the card image, handling the front and back sprites and flipping the card.
+    /// </summary>
     public class CardImageController : GameController<CardView>
     {
         #region Services
 
+        /// <summary>
+        /// Service for managing addressable assets.
+        /// </summary>
         [Inject] private IGameAddressablesService _gameAddressablesService;
 
         #endregion
-        
+
         #region Lifecycle
-        
+
+        /// <summary>
+        /// Initializes the card image controller.
+        /// </summary>
         private void Start()
         {
-            //Get model
+            // Get model
             var model = GetModel<CardModel>();
             model.FrontSprite = GetFrontSprite(model.CardShape, model.CardType);
             model.BackSprite = GetBackSprite();
@@ -35,6 +44,9 @@ namespace Game.Entities.Card
 
         #region Public Methods
 
+        /// <summary>
+        /// Flips the card image between the front and back sprites.
+        /// </summary>
         public void FlipCard()
         {
             var model = GetModel<CardModel>();
@@ -42,14 +54,24 @@ namespace Game.Entities.Card
         }
 
         #endregion
-        
+
         #region Private Methods
 
+        /// <summary>
+        /// Gets the front sprite for the card based on its shape and type.
+        /// </summary>
+        /// <param name="cardShape">The shape of the card.</param>
+        /// <param name="cardType">The type of the card.</param>
+        /// <returns>The front sprite of the card.</returns>
         private Sprite GetFrontSprite(CardShape cardShape, CardType cardType)
         {
             return _gameAddressablesService.GetCardSprite(cardShape, cardType);
         }
-        
+
+        /// <summary>
+        /// Gets the back sprite for the card.
+        /// </summary>
+        /// <returns>The back sprite of the card.</returns>
         private Sprite GetBackSprite()
         {
             return _gameAddressablesService.GetCardSprite(CardShape.Back);
