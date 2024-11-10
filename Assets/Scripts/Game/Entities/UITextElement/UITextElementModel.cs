38857c03a0ff65e5f873836f4b4a2b9d3f4f0fdd
josemgmz <jmgc92@gmail.com>
2024-11-10 11:33:33 +0100

    feat: add UITextElement that will display the game state with properties like score, time, turns and combo.
    (cherry picked from commit 74500696c935933403c7d3cf24c2cb94971b4ded)

---
+++ Assets/Scripts/Game/Entities/UITextElement/UITextElementModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Framework;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Entities.UITextElement
+{
+    [Serializable]
+    public class UITextElementModel : GameModel
+    {
+        #region Variables
+
+        [SerializeField] private TextMeshProUGUI _title;
+        [SerializeField] private TextMeshProUGUI _value;
+
+        #endregion
+
+        #region Properties
+
+        public TextMeshProUGUI Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public TextMeshProUGUI Value
+        {
+            get => _value;
+            set => _value = value;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Game/Entities/UITextElement/UITextElementView.cs
@@ -0,0 +1,15 @@
+﻿using Framework;
+using UnityEngine;
+
+namespace Game.Entities.UITextElement
+{
+    public class UITextElementView : GameViewUI
+    {
+        #region Variables
+
+        [GameFieldAttributes.ModelField, SerializeField] private UITextElementModel _model;
+        [GameFieldAttributes.ControllerField] private UITextElementController _controller;
+
+        #endregion
+    }
+}
