873635c4039233639ee168a3df2145dbc19b9feb
josemgmz <jmgc92@gmail.com>
2024-11-11 00:47:45 +0100

    docs: UITextElementController.cs documentation

--- Assets/Scripts/Game/Entities/UITextElement/UITextElementController.cs
+++ Assets/Scripts/Game/Entities/UITextElement/UITextElementController.cs
@@ -2,20 +2,31 @@
 
 namespace Game.Entities.UITextElement
 {
+    /// <summary>
+    /// Controls the UI text element by setting its title and value.
+    /// </summary>
     public class UITextElementController : GameController<UITextElementView>
     {
         #region Public Methods
-        
+
+        /// <summary>
+        /// Sets the title of the UI text element.
+        /// </summary>
+        /// <param name="title">The title to set.</param>
         public void SetTitle(string title)
         {
             GetModel<UITextElementModel>().Title.text = title;
         }
-        
+
+        /// <summary>
+        /// Sets the value of the UI text element.
+        /// </summary>
+        /// <param name="value">The value to set.</param>
         public void SetValue(string value)
         {
             GetModel<UITextElementModel>().Value.text = value;
         }
-        
+
         #endregion
     }
 }
