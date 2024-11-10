52ddf2823cf72d6650e4512da19b2b073c9bc340
josemgmz <jmgc92@gmail.com>
2024-11-11 00:44:00 +0100

    docs: helpers documentation

--- Assets/Scripts/Game/Helper/TransformExtension.cs
+++ Assets/Scripts/Game/Helper/TransformExtension.cs
@@ -3,8 +3,19 @@ using UnityEngine;
 
 namespace Game.Helper
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="Transform"/>.
+    /// </summary>
     public static class TransformExtension
     {
+        /// <summary>
+        /// Rotates the transform from the specified start rotation to the end rotation over the given duration.
+        /// </summary>
+        /// <param name="transform">The transform to rotate.</param>
+        /// <param name="from">The starting rotation.</param>
+        /// <param name="to">The ending rotation.</param>
+        /// <param name="duration">The duration of the rotation in seconds.</param>
+        /// <returns>An enumerator that performs the rotation over time.</returns>
         public static IEnumerator RotateTo(this Transform transform, Quaternion from, Quaternion to, float duration)
         {
             var elapsedTime = 0.0f;
