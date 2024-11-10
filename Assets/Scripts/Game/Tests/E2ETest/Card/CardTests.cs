707364e76d043b2a29b549fedb6e4b9fd2147daf
josemgmz <jmgc92@gmail.com>
2024-11-10 23:41:26 +0100

    fix: flipCard E2E test

--- Assets/Scripts/Game/Tests/E2ETest/Card/CardTests.cs
+++ Assets/Scripts/Game/Tests/E2ETest/Card/CardTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using Game.Entities.Card;
+using Game.Entities.Card.Data;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -28,21 +30,33 @@ namespace Game.Tests.E2ETest.Card
         [UnityTest]
         public IEnumerator FlipCardTest()
         {
+            
+            //Instantiate the card prefab
             var card = GameObject.Find(CARD_OBJECT);
             var transform = card.transform;
-            var startRotation = transform.rotation;
-            var endRotation = transform.rotation * Quaternion.Euler(0, -180, 0);
+            
+            //Setup the card type and shape
+            var cardView = card.GetComponent<CardView>();
+            cardView.Initialize<CardModel>(CardShape.Heart, CardType.King, false);
+            cardView.enabled = true;
+            
+            
+            //Start the test
             yield return new WaitForSeconds(2.0f);
+            //Setup the rotation of the card
+            var startRotation = transform.rotation;
+            var endRotation = startRotation * Quaternion.Euler(0, -180, 0);
+            
             yield return PerformDrag(transform.position, transform.position + Vector3.down, 0.25f, RenderMode.ScreenSpaceCamera);
             yield return new WaitForSeconds(1.0f);
+            
             //Compare the rotation of the card to the original rotation it should be flipped 180 degrees
             Assert.AreEqual(endRotation.eulerAngles.y, transform.rotation.eulerAngles.y);
             Debug.Log("Card Flipped");
             
-            
             yield return PerformDrag(transform.position, transform.position + Vector3.down, 0.25f, RenderMode.ScreenSpaceCamera);
             yield return new WaitForSeconds(1.0f);
-            Assert.AreEqual(startRotation.eulerAngles.y, transform.rotation.eulerAngles.y);
+            Assert.AreEqual(endRotation.eulerAngles.y, transform.rotation.eulerAngles.y);
             Debug.Log("Card Flipped Back");
             yield return null;
             Debug.Log("Test Complete");
